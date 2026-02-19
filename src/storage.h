f36151183fbbbeb5907a00e208477b151f97a8fc
Abdullah Novruzlu <novruzluabdullah03@gmail.com>
2026-02-19 17:35:01 +0400

    added storage header file

---
+++ src/storage.h
@@ -0,0 +1,16 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include "constants.h"
+#include "pager.h"
+
+typedef struct {
+    Pager *pager;
+    offset_t next_offset;
+} Storage;
+
+Storage *storage_create(Pager *pager);
+DB_Result storage_write(Storage *storage, const void *data, size_t size, page_num_t *page, offset_t *offset);
+DB_Result storage_read(Storage *storage, page_num_t page, offset_t offset, void *buffer, size_t *size);
+
+#endif
